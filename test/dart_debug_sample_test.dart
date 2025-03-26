03c67210ceefef486260abb4a78e7f18642a21bc
cho1taeho <cow12dog@gmail.com>
2025-03-26 16:51:07 +0900

    250326 과제2번

--- test/dart_debug_sample_test.dart
+++ test/dart_debug_sample_test.dart
@@ -2,7 +2,11 @@ import 'package:dart_debug_sample/dart_debug_sample.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('', () {
+  test('그게머시중헌디', () {
+    YukymController yukymController = YukymController();
+    print(yukymController.getTyA()); // 해당 월에 맞는 자시의 국 표시
+    print(yukymController.getTyB()); // 해당 시간에 맞는 자시의 국 표시
+    print(yukymController.nowDate);
 
   });
 }
